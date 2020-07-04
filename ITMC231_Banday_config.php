ed07ffa86829258f088071607fb01621c674974e
TheToinkz <jbanday@gbox.adnu.edu.ph>
2020-07-04 19:57:27 +0800

    Filename changed

--- ITMC231_Banday_config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-	$server = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "bankbookdb";
-
-    $conn = new mysqli($server, $username, $password, $dbname);
-?>
